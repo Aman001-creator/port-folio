3cfc48239e19f3409220bd553b5b7728e4071241
Aman001-creator <119519058+Aman001-creator@users.noreply.github.com>
2024-02-15 01:17:38 +0530

    portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,131 @@
+//active link hover script
+document.addEventListener("DOMContentLoaded", function () {
+    let navLinks = document.querySelectorAll('.navbar-nav a');
+
+    window.addEventListener('scroll', function () {
+        let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+        navLinks.forEach(link => {
+            let sectionId = link.getAttribute('href').substring(1);
+            let section = document.getElementById(sectionId);
+
+            if (section.offsetTop <= scrollPosition && section.offsetTop + section.offsetHeight > scrollPosition) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    });
+
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+
+            let offset = 0;
+            if (window.innerWidth <= 768) {
+                offset = document.querySelector('.navbar').offsetHeight;
+            } else {
+                offset = document.querySelector('.navbar').offsetHeight - 55;
+            }
+
+            window.scrollTo({
+                top: targetSection.offsetTop + offset,
+                behavior: 'smooth'
+            });
+
+
+            navLinks.forEach(navLink => {
+                navLink.classList.remove('active');
+            });
+
+            this.classList.add('active');
+        });
+    });
+});
+
+
+
+
+//top script
+
+document.addEventListener("DOMContentLoaded",function(){
+    var scrollTopBtn = document.getElementById("scrollTopBtn");
+
+    window.onscroll = function(){
+        toggleScrollTopButton();
+};
+
+function toggleScrollTopButton(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop >20){
+        scrollTopBtn.style.display = "block";
+    }
+    else{
+        scrollTopBtn.style.display = "none";
+    }
+}
+
+    function scrollToTop(){
+        window.scrollTo({top:0, behaviour:"smooth"});
+    }
+    if(scrollTopBtn){
+        scrollTopBtn.onclick = scrollToTop;
+    }
+});
+
+//scroll reveal script
+ScrollReveal({ 
+    reset: true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
+ScrollReveal().reveal('.heading1', { origin:'bottom' });
+ScrollReveal().reveal('.home-img, .project-box, .form-group', { origin:'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin:'right' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin:'left' });
+ScrollReveal().reveal('.progress,.skill h3,.skill h6', { origin:'left' });
+
+//typed js
+const typed = new Typed('.multiple-text',{
+    strings: ['Frontend Developer','Designer','Backend Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+//email script
+const form = document.querySelector('form');
+const fullname = document.getElementById('fullname');
+const email = document.getElementById('email');
+const mobile = document.getElementById('mobile');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+
+function sendEmail(){
+    const bodyMessage = `Full Name: ${fullname.value}<br> Email: ${email.value}<br> Phone Number: ${mobile.value}<br> Message: ${message.value}`;
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "itsakpro1@gmail.com",
+        Password : "1B4D52693310A1EE88DBBF92AAA4C248EF67",
+        To : 'itsakpro1@gmail.com',
+        From : "itsakpro1@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => alert('Message sent sucessfully!')
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendEmail();
+});
---
+++ readme.md
@@ -0,0 +1,92 @@
+# Itzak - Portfolio
+
+This is the portfolio of Aman Kumar, a Full Stack Developer, showcasing skills, projects, and contact information.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Sections](#sections)
+  - [Home](#home)
+  - [About](#about)
+  - [Projects](#projects)
+  - [Skills](#skills)
+  - [Contact](#contact)
+- [Technologies Used](#technologies-used)
+- [File Structure](#file-structure)
+- [Usage](#usage)
+- [Dependencies](#dependencies)
+- [External Influences](#external-influences)
+- [License](#license)
+
+## Introduction
+
+This portfolio website serves as a showcase of Aman Kumar's skills, projects, and contact information. It is designed to provide an overview of his capabilities as a Full Stack Developer.
+
+## Sections
+
+### Home
+
+The home section introduces Aman Kumar and provides a brief overview of his skills and personality.
+
+### About
+
+The about section delves into Aman's background as a Full Stack Developer, emphasizing his passion for coding and commitment to creating impactful web experiences.
+
+### Projects
+
+The projects section highlights some of Aman's latest projects, including brief descriptions and external links for further exploration.
+
+### Skills
+
+In the skills section, Aman lists his technical and soft skills along with progress bars to indicate proficiency levels.
+
+### Contact
+
+The contact section includes a form for visitors to reach out to Aman Kumar. It gathers information such as full name, email, mobile number, subject, and message.
+
+## Technologies Used
+
+- [HTML5]: Used for structure of website.
+- [CSS3]: Used for the style of the website.
+- [Bootstrap4]: Used for the responsive design of website.
+- [Javascript]: Used for the logics behind the website.
+
+## File Structure
+
+- **assets/**
+  - **css/**: Contains the stylesheet (`style.css`).
+  - **font-awesome-4.7.0/**: Includes Font Awesome 4.7.0 files.
+  - **images/**: Holds images used in the portfolio.
+  - **js/**: Stores JavaScript files (`script.js`).
+  - **favicon/**: Contains the favicon image (`favicon-96x96.png`).
+  - **cv.pdf**: A downloadable resume.
+- **README.md**: The document you're currently reading.
+- **index.html**: The main HTML file for the portfolio.
+
+## Usage
+
+Feel free to use this portfolio template as a starting point for your own portfolio. Customize the content, images, and styles to reflect your personal and professional information.
+
+## Dependencies
+
+- Bootstrap 4.6.2: [Bootstrap](https://getbootstrap.com/)
+- jQuery 3.6.3: [jQuery](https://jquery.com/)
+- Popper.js 1.16.1: [Popper.js](https://popper.js.org/)
+- ScrollReveal: [ScrollReveal](https://scrollrevealjs.org/)
+- Typed.js 2.1.0: [Typed.js](https://github.com/mattboldt/typed.js/)
+- SMTP.js: [SMTP.js](https://smtpjs.com/)
+
+## External Influences
+
+Tutorials from CodeHal YouTube Channel: Some design and coding concepts were inspired by tutorials from CodeHal's channel.
+Gemini: Used for the image generation.
+Canva: Used to design the portfolio.
+other resources......
+
+## License
+
+This portfolio is licensed under the (LICENSE).
+
+--------------------------------------------------------
+
+Copyright ©️ 2024 - Itzak portfolio | All rights reserved.
